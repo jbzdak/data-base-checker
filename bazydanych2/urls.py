ac3165447c9de23d4103c0f533be150e0ca8cf81
Jacek Bzdak <jb@debian>
2013-09-09 19:11:04 +0200

    Removed bdchecker

--- bazydanych2/urls.py
+++ bazydanych2/urls.py
@@ -5,6 +5,5 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^grading/', include("grading.urls")),
-    url(r'^bdchecker/', include("bdchecker.urls")),
+    url(r'^grading/', include("grading.urls"))
 )
