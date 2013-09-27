8db0020be0c69b5892a607d8894b01e079303cb9
Jacek Bzdak <jbzdak@gmail.com>
2013-09-27 22:20:46 +0200

    All tests now pass.

---
+++ grading/migrations/0012_auto__chg_field_course_slug_field__chg_field_gradeableactivity_slug_fi.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Course.slug_field'
+        db.alter_column('grading_course', 'slug_field', self.gf('django.db.models.fields.SlugField')(unique=True, null=True, max_length=100))
+
+        # Changing field 'GradeableActivity.slug_field'
+        db.alter_column('grading_gradeableactivity', 'slug_field', self.gf('django.db.models.fields.SlugField')(unique=True, null=True, max_length=100))
+
+        # Changing field 'GradePart.slug_field'
+        db.alter_column('grading_gradepart', 'slug_field', self.gf('django.db.models.fields.SlugField')(unique=True, null=True, max_length=100))
+
+    def backwards(self, orm):
+
+        # Changing field 'Course.slug_field'
+        db.alter_column('grading_course', 'slug_field', self.gf('django.db.models.fields.SlugField')(unique=True, null=True, max_length=50))
+
+        # Changing field 'GradeableActivity.slug_field'
+        db.alter_column('grading_gradeableactivity', 'slug_field', self.gf('django.db.models.fields.SlugField')(unique=True, null=True, max_length=50))
+
+        # Changing field 'GradePart.slug_field'
+        db.alter_column('grading_gradepart', 'slug_field', self.gf('django.db.models.fields.SlugField')(unique=True, null=True, max_length=50))
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'blank': 'True', 'to': "orm['auth.Permission']"})
+        },
+        'auth.permission': {
+            'Meta': {'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission', 'ordering': "('content_type__app_label', 'content_type__model', 'codename')"},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'blank': 'True', 'max_length': '75'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '30'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'blank': 'True', 'related_name': "'user_set'", 'to': "orm['auth.Group']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '30'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'blank': 'True', 'related_name': "'user_set'", 'to': "orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'", 'ordering': "('name',)"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'grading.autogradeablegradepart': {
+            'Meta': {'_ormbases': ['grading.GradePart'], 'object_name': 'AutogradeableGradePart', 'ordering': "('sort_key',)"},
+            'autograding_controller': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'parent': ('django.db.models.fields.related.OneToOneField', [], {'unique': 'True', 'related_name': "'autograde'", 'primary_key': 'True', 'to': "orm['grading.GradePart']"})
+        },
+        'grading.autogradingresult': {
+            'Meta': {'object_name': 'AutogradingResult', 'ordering': "['save_date']"},
+            'autograder_input_content_type': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'null': 'True', 'to': "orm['contenttypes.ContentType']"}),
+            'autograder_input_pk': ('django.db.models.fields.PositiveIntegerField', [], {'blank': 'True', 'null': 'True'}),
+            'grade': ('django.db.models.fields.DecimalField', [], {'decimal_places': '2', 'max_digits': '5'}),
+            'grade_part': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['grading.GradePart']"}),
+            'grading_result': ('picklefield.fields.PickledObjectField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'long_description': ('django.db.models.fields.TextField', [], {'blank': 'True', 'null': 'True'}),
+            'partial_grade': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'autogrades'", 'null': 'True', 'to': "orm['grading.PartialGrade']"}),
+            'save_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True', 'null': 'True'}),
+            'short_description': ('django.db.models.fields.TextField', [], {'blank': 'True', 'null': 'True'}),
+            'student': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['grading.Student']"})
+        },
+        'grading.booleaninput': {
+            'Meta': {'object_name': 'BooleanInput'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user_input': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'grading.course': {
+            'Meta': {'object_name': 'Course', 'ordering': "('sort_key',)"},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'slug_field': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'blank': 'True', 'null': 'True', 'max_length': '100'}),
+            'sort_key': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '100'})
+        },
+        'grading.gradeableactivity': {
+            'Meta': {'object_name': 'GradeableActivity'},
+            'courses': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'activities'", 'to': "orm['grading.Course']"}),
+            'default_grade': ('django.db.models.fields.DecimalField', [], {'default': '2.0', 'decimal_places': '2', 'max_digits': '5'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug_field': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'blank': 'True', 'null': 'True', 'max_length': '100'}),
+            'sort_key': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '100'})
+        },
+        'grading.gradepart': {
+            'Meta': {'unique_together': "[('activity', 'name')]", 'object_name': 'GradePart', 'ordering': "['sort_key']"},
+            'activity': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'grade_parts'", 'to': "orm['grading.GradeableActivity']"}),
+            'default_grade': ('django.db.models.fields.DecimalField', [], {'default': '2.0', 'decimal_places': '2', 'max_digits': '5'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'passing_grade': ('django.db.models.fields.DecimalField', [], {'default': '3.0', 'decimal_places': '2', 'max_digits': '5'}),
+            'required': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'slug_field': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'blank': 'True', 'null': 'True', 'max_length': '100'}),
+            'sort_key': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '100'}),
+            'weight': ('django.db.models.fields.DecimalField', [], {'default': '1', 'decimal_places': '2', 'blank': 'True', 'max_digits': '5'})
+        },
+        'grading.gradingbooleaninput': {
+            'Meta': {'object_name': 'GradingBooleanInput'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'required_input': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'user_input': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'grading.gradingtextinput': {
+            'Meta': {'object_name': 'GradingTextInput'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user_input': ('django.db.models.fields.TextField', [], {})
+        },
+        'grading.partialgrade': {
+            'Meta': {'unique_together': "(('student', 'grade_part'),)", 'object_name': 'PartialGrade', 'ordering': "['save_date']"},
+            'grade': ('django.db.models.fields.DecimalField', [], {'decimal_places': '2', 'max_digits': '5'}),
+            'grade_part': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['grading.GradePart']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'long_description': ('django.db.models.fields.TextField', [], {'blank': 'True', 'null': 'True'}),
+            'save_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True', 'null': 'True'}),
+            'short_description': ('django.db.models.fields.TextField', [], {'blank': 'True', 'null': 'True'}),
+            'student': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['grading.Student']"})
+        },
+        'grading.student': {
+            'Meta': {'object_name': 'Student', 'ordering': "('user__last_name', 'user__first_name', 'user__email', 'user__pk')"},
+            'course': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'students'", 'null': 'True', 'to': "orm['grading.Course']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'student_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'null': 'True', 'max_length': '100'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'unique': 'True', 'related_name': "'student'", 'to': "orm['auth.User']"})
+        },
+        'grading.studentgrade': {
+            'Meta': {'object_name': 'StudentGrade'},
+            'activity': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['grading.GradeableActivity']"}),
+            'grade': ('django.db.models.fields.DecimalField', [], {'decimal_places': '2', 'max_digits': '5'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'long_description': ('django.db.models.fields.TextField', [], {'blank': 'True', 'null': 'True'}),
+            'short_description': ('django.db.models.fields.TextField', [], {'blank': 'True', 'null': 'True'}),
+            'student': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'grades'", 'to': "orm['grading.Student']"})
+        }
+    }
+
+    complete_apps = ['grading']
