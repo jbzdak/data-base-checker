2a1587dda4efad8d3b4e329855a212b7c81e4f0e
Jacek Bzdak <jbzdak@gmail.com>
2013-10-23 18:51:58 +0200

    ...

--- bdcheckerapp/management/commands/dump_activity.py
+++ bdcheckerapp/management/commands/dump_activity.py
@@ -31,7 +31,7 @@ class Command(BaseCommand):
             agp = AutogradeableGradePart.objects.filter(activity=activity)
             grade_parts = list()
             ids = agp.values_list("pk")
-            grade_parts.extend(GradePart.objects.filter(~Q(pk__in=ids,activity=activity)))
+            grade_parts.extend(GradePart.objects.filter(activity=activity))
             grade_parts.extend(agp)
             grade_parts.append(activity)
             json_serializer.serialize(grade_parts, stream=str)
